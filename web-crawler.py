f19d933c4eb752410206e25e03a8d742e49d2489
furuku77 <furuku_mini_black@hotmail.com>
2024-06-09 11:47:23 +0700

    🍱 add beauty excel

--- web-crawler.py
+++ web-crawler.py
@@ -13,6 +13,9 @@ import pandas as pd
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
+from openpyxl import Workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
+from openpyxl.styles import PatternFill, Font, Border, Side
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
@@ -89,16 +92,65 @@ def process_bigc_data(context: AssetExecutionContext, get_page_contents: list):
 
     df = pd.DataFrame(product_data, columns=["Product Name", "Price"])
 
+    return df
+
+
+@asset
+def beauty_excel(context: AssetExecutionContext, process_bigc_data: pd.DataFrame):
+
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "product_price"
+
+    # Define header fill and font styles
+    header_fill = PatternFill(
+        start_color="FFFF00", end_color="FFFF00", fill_type="solid"
+    )
+    header_font = Font(bold=True)
+
+    # Define border style
+    thin_border = Border(
+        left=Side(style="thin"),
+        right=Side(style="thin"),
+        top=Side(style="thin"),
+        bottom=Side(style="thin"),
+    )
+
+    for r_idx, row in enumerate(
+        dataframe_to_rows(process_bigc_data, index=False, header=True), 1
+    ):
+        for c_idx, value in enumerate(row, 1):
+            cell = ws.cell(row=r_idx, column=c_idx, value=value)
+            if r_idx == 1:
+                cell.fill = header_fill
+                cell.font = header_font
+
+            # Apply border to each cell
+            cell.border = thin_border
+
+    # Set the column width to a fixed width or based on the max length of content in each column
+    for col in ws.columns:
+        max_length = 0
+        column = col[0].column_letter  # Get the column name
+        for cell in col:
+            try:
+                if len(str(cell.value)) > max_length:
+                    max_length = len(cell.value)
+            except:
+                pass
+        adjusted_width = max_length + 2
+        ws.column_dimensions[column].width = adjusted_width
+
     # filename with date and time
-    today = pd.Timestamp.now().strftime("%Y-%m-%d_%H-%M-%S")
+    today = pd.Timestamp.now().strftime("%Y%m%d_%H-%M-%S")
     filename = f"product_bigc_{today}.xlsx"
-    df.to_excel("./data/" + filename, index=False)
+    wb.save("./data/" + filename)
 
     return filename
 
 
 @asset
-def upload_file_drive(context: AssetExecutionContext, process_bigc_data: str):
+def upload_file_drive(context: AssetExecutionContext, beauty_excel: str):
     # Define the Google Drive API scopes and service account file path
     SCOPES = ["https://www.googleapis.com/auth/drive"]
     SERVICE_ACCOUNT_FILE = "./credential.json"
@@ -115,12 +167,12 @@ def upload_file_drive(context: AssetExecutionContext, process_bigc_data: str):
 
     # Upload the file to the destination folder
     file_metadata = {
-        "name": process_bigc_data,
+        "name": beauty_excel,
         "parents": [destination_folder_id],  # Specify the destination folder ID
     }
 
     media = MediaFileUpload(
-        "./data/" + process_bigc_data,
+        "./data/" + beauty_excel,
         mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
     )
     file = (
@@ -137,6 +189,7 @@ defs = Definitions(
         get_page_contents,
         process_bigc_data,
         upload_file_drive,
+        beauty_excel,
     ],
     jobs=[
         define_asset_job(
